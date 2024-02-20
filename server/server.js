1f0c2cad35ecf8384481c91372ccd59d070eae65
somu878 <somumydhili19@gmail.com>
2024-02-20 11:46:17 +0530

    web socket test #1

--- server/server.js
+++ server/server.js
@@ -2,44 +2,50 @@ const express = require("express");
 require("dotenv").config();
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
+const db = require("./db/config");
+const authRouter = require("./routes/authRoute");
+const cardRouter = require("./routes/cardRoute");
+const origin = process.env.CLIENT_ORIGIN || "http://localhost:5173";
+
 const app = express();
-var cors = require("cors");
-const origin = process.env.CLIENT_ORIGIN;
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["*"],
+    methods: ["GET", "POST", "UPDATE", "DELETE", "PATCH"],
+  },
+});
+
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["*"],
+    credentials: true,
+  })
+);
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
-
   next();
 });
-const corsConfig = {
-  origin: ["*"],
-  credentials: true,
-};
-app.use(cors(corsConfig));
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: [process.env.CLIENT_ORIGIN, "http://localhost:5173"],
-    methods: ["GET", "POST", "UPDATE", "DELETE", "PATCH"],
-  },
-});
-const db = require("./db/config");
-const authRouter = require("./routes/authRoute");
-const cardRouter = require("./routes/cardRoute");
-const port = process.env.PORT || 9000;
-app.use(express.json());
+
 app.use("/api/v1/user", authRouter);
 app.use("/api/v1/card", cardRouter);
 
 io.on("connection", (socket) => {
-  console.log(`A user connected : ${socket.id}`);
+  console.log(`A user connected: ${socket.id}`);
+  socket.emit("yourCustomEvent", { message: "Hello from the server!" });
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
 });
-app.listen(port, () => {
-  console.log(`Listening in port ${port}`);
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
